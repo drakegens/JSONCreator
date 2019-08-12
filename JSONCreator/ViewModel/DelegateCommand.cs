85da8bf6731a7df4a377bb61442d2def258e63de
Drake Gens <drake.gens@gmail.com>
2019-08-12 13:40:29 -0500

    Adding Binding

---
+++ JSONCreator/ViewModel/DelegateCommand.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Input;
+using System;
+
+namespace JSONCreator.ViewModel
+{
+    public class DelegateCommand : ICommand
+    {
+        private readonly Action _action;
+
+        public DelegateCommand(Action action)
+        {
+            _action = action;
+        }
+
+        public void Execute(object parameter)
+        {
+            _action();
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+    }
+}
---
+++ JSONCreator/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using JSONCreator.BusinessLogic;
+
+namespace JSONCreator.ViewModel
+{
+    class MainWindowViewModel : ObservableObject
+    {
+        private JSONObjectCreator _jsonCreator = new JSONObjectCreator();
+
+
+        private string name1;
+
+        public string Name1
+        {
+            get => this.name1;
+            set
+            {
+                name1 = value;
+               RaisePropertyChangedEvent("propertyName");
+            }
+        }
+    }
+}
